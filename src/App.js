ce28c47ca85947dff2902d6b28552edcce6488cb
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-10 11:29:18 -0500

    imported useState hook and set up a state property for the team member list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
 
 function App() {
+  const [member, newMember] = useState([
+    {
+      name: "name",
+      email: "email",
+      role: "role"
+    }
+  ]);
+
   return (
     <div className="App">
       <Form />
+      <Member member={member} />
     </div>
   );
 }
